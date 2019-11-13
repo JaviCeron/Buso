176ab7533a554d21bf93edd25521218f709d4e56
Elizabeth <elizabethalvarezgra17@gmail.com>
2019-11-13 13:56:48 -0600

    Agrege la base de datos original, con los cambios correspondientes.Lista para usarse

--- BD/buso(DB actualizada)/mybuso.sql
+++ BD/buso(DB actualizada)/mybuso.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-11-2019 a las 02:55:40
--- Versión del servidor: 10.4.8-MariaDB
--- Versión de PHP: 7.3.11
+-- Tiempo de generación: 13-11-2019 a las 20:52:40
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,29 +25,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `bus`
---
-
-CREATE TABLE `bus` (
-  `idbus` int(10) NOT NULL,
-  `numero_ruta` varchar(10) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `nombre_bus` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `tarifa` varchar(15) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `idterminal` int(15) NOT NULL,
-  `idhorario` int(15) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `horario`
 --
 
 CREATE TABLE `horario` (
-  `idhorario` int(15) NOT NULL,
+  `idhorario` int(11) NOT NULL,
   `hora_salida` time NOT NULL,
-  `hora_meta` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `hora_meta` time NOT NULL,
+  `idruta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -56,28 +42,34 @@ CREATE TABLE `horario` (
 --
 
 CREATE TABLE `terminal` (
-  `idterminal` int(15) NOT NULL,
-  `terminal_salida` varchar(150) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `terminal_meta` varchar(150) COLLATE utf8mb4_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `idterminal` int(11) NOT NULL,
+  `nombre_terminal` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `apellido` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+  `clave` varchar(300) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `bus`
---
-ALTER TABLE `bus`
-  ADD PRIMARY KEY (`idbus`),
-  ADD KEY `fk_idterminal` (`idterminal`),
-  ADD KEY `fk_idhorario` (`idhorario`);
-
---
 -- Indices de la tabla `horario`
 --
 ALTER TABLE `horario`
-  ADD PRIMARY KEY (`idhorario`);
+  ADD PRIMARY KEY (`idhorario`),
+  ADD KEY `idruta` (`idruta`);
 
 --
 -- Indices de la tabla `terminal`
@@ -90,33 +82,16 @@ ALTER TABLE `terminal`
 --
 
 --
--- AUTO_INCREMENT de la tabla `bus`
---
-ALTER TABLE `bus`
-  MODIFY `idbus` int(10) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `horario`
 --
 ALTER TABLE `horario`
-  MODIFY `idhorario` int(15) NOT NULL AUTO_INCREMENT;
+  MODIFY `idhorario` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `terminal`
 --
 ALTER TABLE `terminal`
-  MODIFY `idterminal` int(15) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `bus`
---
-ALTER TABLE `bus`
-  ADD CONSTRAINT `fk_idhorario` FOREIGN KEY (`idhorario`) REFERENCES `horario` (`idhorario`),
-  ADD CONSTRAINT `fk_idterminal` FOREIGN KEY (`idterminal`) REFERENCES `terminal` (`idterminal`);
+  MODIFY `idterminal` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
