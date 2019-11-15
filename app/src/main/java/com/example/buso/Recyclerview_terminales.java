2171236a462bbeaecc4a364cb9d3c51acf559715
javiergale <javiergaleanomendoza1999@gmail.com>
2019-11-15 12:08:12 -0600

    Comienzo con JSOPN  Array

--- app/src/main/java/com/example/buso/Recyclerview_terminales.java
+++ app/src/main/java/com/example/buso/Recyclerview_terminales.java
@@ -8,10 +8,15 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
 import com.example.buso.Entidades.Terminal;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class Recyclerview_terminales extends AppCompatActivity {
     List<Terminal> terminalList;
@@ -49,6 +54,21 @@ public class Recyclerview_terminales extends AppCompatActivity {
         rvTerminales.setLayoutManager(new LinearLayoutManager(this));
         terminalList = new ArrayList<>();
 
+        loadTerminales();
+    }
+
+    public void loadTerminales(){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
 
     }
 }
