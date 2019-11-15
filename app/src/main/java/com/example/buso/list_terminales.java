2a94c4f6aadd3ed8a1ef2a8b63b9961bb9875c3c
javiergale <javiergaleanomendoza1999@gmail.com>
2019-11-14 21:06:32 -0600

    cree el layout de consulta de terminales

---
+++ app/src/main/java/com/example/buso/list_terminales.java
@@ -0,0 +1,14 @@
+package com.example.buso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_terminales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_terminales);
+    }
+}
