aa84bd77171f98741815624c344a3ad678644c98
javiergale <javiergaleanomendoza1999@gmail.com>
2019-11-15 15:59:13 -0600

    Solucion de problemas

--- app/src/main/java/com/example/buso/TerminalesAdapter.java
+++ app/src/main/java/com/example/buso/TerminalesAdapter.java
@@ -1,12 +1,15 @@
 package com.example.buso;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.buso.Entidades.Terminal;
 
 import java.util.List;
@@ -23,12 +26,21 @@ public class TerminalesAdapter extends RecyclerView.Adapter<TerminalesAdapter.Pr
     @NonNull
     @Override
     public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater =LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.rv_lista_terminales, null);
+        return new ProductViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+    Terminal terminal =terminalList.get(position);
 
+    String ts = terminal.getNombreTerminal();
+    if (ts.isEmpty()){
+        holder.tvNombreTerminal.setText(String.valueOf(terminal.getNombreTerminal()));
+    }else {
+        holder.tvNombreTerminal.setText(String.valueOf(terminal.getNombreTerminal()));
+    }
     }
 
     @Override
@@ -37,8 +49,14 @@ public class TerminalesAdapter extends RecyclerView.Adapter<TerminalesAdapter.Pr
     }
 
     public class ProductViewHolder extends RecyclerView.ViewHolder {
+        TextView tvNombreTerminal;
+
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
+
+
+            tvNombreTerminal =itemView.findViewById(R.id.tvNombreTerminal);
+
         }
     }
 }
