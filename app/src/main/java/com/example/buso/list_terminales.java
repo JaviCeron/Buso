15f6ac607768559102adf3cc79fed49289258461
javiergale <javiergaleanomendoza1999@gmail.com>
2019-11-14 22:40:09 -0600

    Procedo a trabajar en la case del ListView

--- app/src/main/java/com/example/buso/list_terminales.java
+++ app/src/main/java/com/example/buso/list_terminales.java
@@ -3,9 +3,12 @@ package com.example.buso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class list_terminales extends AppCompatActivity {
-
+        private TextView tv1;
+        private ListView l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
