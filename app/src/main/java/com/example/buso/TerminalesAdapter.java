e70e10836347ef21818fe4e63ceaaca533d6988f
javiergale <javiergaleanomendoza1999@gmail.com>
2019-11-15 14:50:28 -0600

    Constructor de adaptador

--- app/src/main/java/com/example/buso/TerminalesAdapter.java
+++ app/src/main/java/com/example/buso/TerminalesAdapter.java
@@ -1,12 +1,25 @@
 package com.example.buso;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.buso.Entidades.Terminal;
+
+import java.util.List;
+
 public class TerminalesAdapter extends RecyclerView.Adapter<TerminalesAdapter.ProductViewHolder> {
+    private Context context;
+    private List<Terminal> terminalList;
+
+    public TerminalesAdapter(Context context, List<Terminal> terminalList) {
+        this.context = context;
+        this.terminalList = terminalList;
+    }
+
     @NonNull
     @Override
     public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
