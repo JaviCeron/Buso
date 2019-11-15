ace1208aa57fb478e88d3317d0e41e3d559eaa70
javiergale <javiergaleanomendoza1999@gmail.com>
2019-11-15 11:53:13 -0600

    Avance en RV

--- app/src/main/java/com/example/buso/Recyclerview_terminales.java
+++ app/src/main/java/com/example/buso/Recyclerview_terminales.java
@@ -1,17 +1,43 @@
 package com.example.buso;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import com.example.buso.Entidades.Terminal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Recyclerview_terminales extends AppCompatActivity {
     List<Terminal> terminalList;
     RecyclerView rvTerminales;
+    TerminalesAdapter adapter;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Buso")
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+            return true;
+        }
+        //para las demas cosas, se reenvia el evento al listener habitual
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +45,10 @@ public class Recyclerview_terminales extends AppCompatActivity {
         setContentView(R.layout.activity_recyclerview_terminales);
 
             rvTerminales = findViewById(R.id.rvTerminales);
+        rvTerminales.setHasFixedSize(true);
+        rvTerminales.setLayoutManager(new LinearLayoutManager(this));
+        terminalList = new ArrayList<>();
+
+
     }
 }
