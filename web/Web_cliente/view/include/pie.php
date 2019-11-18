48e0c2e0327d433002ee674828db462d3dd271be
ElenaBarahona <50681278+ElenaBarahona@users.noreply.github.com>
2019-11-18 11:16:00 -0600

    modificacion de pie.php cliente

--- web/Web_cliente/view/include/pie.php
+++ web/Web_cliente/view/include/pie.php
@@ -2,12 +2,9 @@
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
-          <h5 class="white-text">Buso</h5>
-          <p class="grey-text text-lighten-4">Desarrollo de aplicaciones para la Web.<br>
-          ITCA-FEPADE Regional Zacatecoluca.<br>
-          CicloII-2019.</p>
-
-
+          <h6 class="white-text">Desarrollo de aplicaciones Android. ITCA-FEPADE Regional Zacatecoluca.
+         CicloII-2019.</h6>
+         
         </div>
         <div class="col l6 s12 center">
          </div>
