f2007594344459cc58a2ad5771e9fa2c3e5490f5
Cernagomez <50711627+Cernagomez@users.noreply.github.com>
2019-11-17 21:47:07 -0600

    lista de buses en la activity

---
+++ app/src/main/java/com/example/buso/Activity_list.java
@@ -0,0 +1,32 @@
+package com.example.buso;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class Activity_list extends AppCompatActivity {
+    ListView listaterminal;
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    listaterminal = (ListView) listaterminal.findViewById(R.id.lv133);
+    ArrayList lista =new ArrayList();
+    lista.add("bus133");
+    lista.add("bus302");
+    lista.add("bus512");
+
+    ArrayAdapter adaptador = new ArrayAdapter(this,android.R.layout.simple_list_item_1, lista);
+    ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this, R.array.Lista, android.R.layout.simple_list_item_1);
+
+    listaterminal.setAdapter(adaptador);
+}
+
+
+    }
