2059a375b4a542cd678cda5a76ff722494fd120d
ElenaBarahona <50681278+ElenaBarahona@users.noreply.github.com>
2019-11-18 09:14:11 -0600

    modifique mostrar_cliente

--- web/WebTerminal/view/mostrar_cliente.php
+++ web/WebTerminal/view/mostrar_cliente.php
@@ -4,7 +4,7 @@
       <!--   Icon Section   -->
       <div class="row">
         <div class="col s12">
-            <h2 class="center grey-text text-darken-4"><i class="medium material-icons">airplay</i> Clientes Registrados</h2>
+            <h2 class="center grey-text text-darken-4"><i class="medium material-icons">account_circle</i> Clientes Registrados</h2>
         </div>
 
              <!-- datos -->
